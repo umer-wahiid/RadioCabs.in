f5f76c38bb3c97d09f274e3829f72156eacc5d97
Umer Abdul Wahid <umerwahiid48@gmail.com>
2023-05-24 14:20:10 +0500

    Listing

--- RadioCabs/RadioCabs/Controllers/HomeController.cs
+++ RadioCabs/RadioCabs/Controllers/HomeController.cs
@@ -46,6 +46,10 @@ namespace RadioCabs.Controllers
 		{
 			return View();
 		}
+		public IActionResult Login()
+		{
+			return View();
+		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
