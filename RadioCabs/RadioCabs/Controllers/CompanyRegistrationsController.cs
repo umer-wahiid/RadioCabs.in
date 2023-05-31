4ca469fd1cda856cebe49f68f9c1164ce9573dee
ansari7016 <ammaransari7016@gmail.com>
2023-05-31 23:24:51 +0500

    commit

--- RadioCabs/RadioCabs/Controllers/CompanyRegistrationsController.cs
+++ RadioCabs/RadioCabs/Controllers/CompanyRegistrationsController.cs
@@ -12,10 +12,12 @@ namespace RadioCabs.Controllers
     public class CompanyRegistrationsController : Controller
     {
         private readonly RCDbContext _context;
+        IWebHostEnvironment iw;
 
-        public CompanyRegistrationsController(RCDbContext context)
+        public CompanyRegistrationsController(RCDbContext context, IWebHostEnvironment i)
         {
             _context = context;
+            iw = i;
         }
 
         // GET: CompanyRegistrations
@@ -91,32 +93,29 @@ namespace RadioCabs.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CompanyId,CompanyName,ContactPerson,Designation,Address,Mobile,Telephone,FaxNumber,Email,MembershipType,PaymentType,City,Description,LogoImage,UserId")] CompanyRegistration companyRegistration)
+        public async Task<IActionResult> Edit(int id, CompanyRegistration companyRegistration, IFormFile image)
         {
-            if (id != companyRegistration.CompanyId)
+            if (image != null)
             {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
+                string ext = Path.GetExtension(image.FileName);
+                if (ext == ".jpg" || ext == ".png")
                 {
+                    string d = Path.Combine(iw.WebRootPath, "Image");
+                    var fname = Path.GetFileName(image.FileName);
+                    string filepath = Path.Combine(d, fname);
+                    using (var fs = new FileStream(filepath, FileMode.Create))
+                    {
+                        await image.CopyToAsync(fs);
+                    }
+                    companyRegistration.LogoImage = @"Image/" + fname;
                     _context.Update(companyRegistration);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateConcurrencyException)
+                else
                 {
-                    if (!CompanyRegistrationExists(companyRegistration.CompanyId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    ViewBag.m = "Wrong Picture Format";
                 }
-                return RedirectToAction(nameof(Index));
             }
             return View(companyRegistration);
         }
