16c52b286b263da82319fa49cf0399ed43b79527
Umer Abdul Wahid <umerwahiid48@gmail.com>
2023-06-18 16:06:10 +0500

    Commit

---
+++ RadioCabs/RadioCabs/Models/AdvertiseRegistrationsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace RadioCabs.Models
+{
+	public class AdvertiseRegistrationsViewComponent : ViewComponent
+    {
+        public RCDbContext _context;
+
+        public AdvertiseRegistrationsViewComponent(RCDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var AdsCompany = await _context.AdvertiseRegistrations.Where(m => m.PaymentStatus == "Received").ToListAsync();
+
+            return View(AdsCompany.ToList());
+        }
+    }
+}
